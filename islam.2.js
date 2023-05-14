afe52a66d0817cdfaeaea69a987de6318ae0c569
hmsho <ahmadmehjazi.mhj@gmail.com>
2023-05-14 07:18:58 +0300

    finally i done popup and got some help
    fixed the nav bar and added a working popup and everything is cool , just need to rewrite it , and test if it work if i make everything the same but dif body . if it dont i might need to make one popup for all

--- islam.2.js
+++ islam.2.js
@@ -1,38 +1,36 @@
-const openModa1Buttons = document.querySelectorAll('[data-modal-target]')
-const closeModa1Buttons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementsById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
-openModa1Buttons.forEach(button => {
-  button.addEventListener('click', () =>{
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
   })
 })
 
-overlay.addEventListener('click',() => {
+overlay.addEventListener('click', () => {
   const modals = document.querySelectorAll('.modal.active')
   modals.forEach(modal => {
-    closemodal(modal)
+    closeModal(modal)
   })
 })
 
-closeModa1Buttons.forEach(button => {
-  button.addEventListener('click', () =>{
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
     const modal = button.closest('.modal')
     closeModal(modal)
   })
 })
 
-function openmodal(modal) {
+function openModal(modal) {
   if (modal == null) return
-    modal.classlist.add('active')
-    overlay.classlist.add('active')
+  modal.classList.add('active')
+  overlay.classList.add('active')
 }
 
-function closemodal(modal) {
+function closeModal(modal) {
   if (modal == null) return
-    modal.classlist.remove('active')
-    overlay.classlist.remove('active')
-
-
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
 }
